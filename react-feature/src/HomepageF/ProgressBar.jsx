10d88cf86c9015596c4ad0e24b5d319763388d05
SuperDex7 <superdexter7@gmail.com>
2025-02-07 20:46:41 -0500

    Profile Preview half complete

---
+++ react-feature/src/HomepageF/ProgressBar.jsx
@@ -0,0 +1,9 @@
+function ProgressBar({ value, max }) {
+    return (
+      <div className="progress-bar">
+        <div className="progress-bar-completed" style={{ width: `${(value / max) * 100}%` }}>
+        </div>
+      </div>
+    );
+  }
+  export default ProgressBar
--- react-feature/src/Pages/Homepage.jsx
+++ react-feature/src/Pages/Homepage.jsx
@@ -1,7 +1,21 @@
 import { useState } from 'react';
+import ProgressBar from '../HomepageF/ProgressBar';
 function Homepage() {
   
     const [loggedIn, setLoggedIn] = useState(false);
+    const [progress, setProgress] = useState(0);
+    const [progCheck, setProgCheck] = useState(true);
+
+    const simulateProgress = () => {
+      setProgress(progress + 10);
+      if(progress >= 100){
+        maxProgress();
+      }
+    };
+    const maxProgress = () => {
+      setProgress(100);
+      setProgCheck(false);
+    }
     const login = () => {
         setLoggedIn(true);
     }
@@ -26,8 +40,28 @@ function Homepage() {
         </ul>
       </nav>
 
-        <div id='profileS'>
+       <div id='profileS'>
+        <section id='profileSi'>
         <h2>SuperDex</h2>
+        <h3>Followers: 0</h3>
+        <h3>Following: 0</h3>
+         </section>
+         <section>
+          <img id='pp' src="dpp.jpg" alt="" />
+          </section>
+          {progCheck && 
+          <section id='progress'>
+            <h2 id='complete'>Complete Profile</h2>
+            <ProgressBar value={progress} max={100} />
+      <button onClick={simulateProgress}>Increase Progress</button>
+          </section>
+          
+          }
+          
+          <section className='upload'>
+            <h3><a href="/upload"></a>Upload a song/beat</h3>
+          </section>
+         
         </div>
     </div>
   );
