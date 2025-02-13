1e3bc3c9fcf6ce529f0d9d21be0ed1571d2587ff
Dexter <114762780+SuperDex7@users.noreply.github.com>
2025-02-13 10:42:32 -0500

    switched from express to spring boot for scalability

---
+++ FeatureMe/src/main/java/Feat/FeatureMe/FeatureMeApplication.java
@@ -0,0 +1,13 @@
+package Feat.FeatureMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeatureMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeatureMeApplication.class, args);
+	}
+
+}
---
+++ FeatureMe/src/test/java/Feat/FeatureMe/FeatureMeApplicationTests.java
@@ -0,0 +1,13 @@
+package Feat.FeatureMe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeatureMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
