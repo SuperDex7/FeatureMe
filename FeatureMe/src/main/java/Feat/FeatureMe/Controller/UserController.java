b7f868268f28dd8f6dbef3320a14ab82907a83aa
SuperDex7 <superdexter7@gmail.com>
2025-06-13 21:29:07 -0400

    Profile Pic and Banner Files Send to AWS

--- FeatureMe/src/main/java/Feat/FeatureMe/Controller/UserController.java
+++ FeatureMe/src/main/java/Feat/FeatureMe/Controller/UserController.java
@@ -2,14 +2,20 @@ package Feat.FeatureMe.Controller;
 
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import Feat.FeatureMe.Dto.UserDTO;
 import Feat.FeatureMe.Entity.User;
+import Feat.FeatureMe.Service.S3Service;
 import Feat.FeatureMe.Service.UserService;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,16 +28,31 @@ import org.springframework.web.bind.annotation.PostMapping;
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
-
+    private final S3Service s3Service;
     private final UserService userService;
     
-    public UserController(UserService userService) {
+    public UserController(UserService userService, S3Service s3Service) {
         this.userService = userService;
-
+        this.s3Service = s3Service;
     }
 
-     @PostMapping("/create")
-    public User createUser(@RequestBody User user) {
+     @PostMapping(path = "/create", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public User createUser(@RequestPart User user,
+    @RequestPart("pp") MultipartFile pp,
+    @RequestPart("banner") MultipartFile banner ) throws IOException {
+        String ppName = pp.getOriginalFilename();
+        String bannerName = banner.getOriginalFilename();
+        File ppTemp = File.createTempFile("pptemp", null);
+        File bannerTemp = File.createTempFile("bannerTemp", null);
+        pp.transferTo(ppTemp);
+        banner.transferTo(bannerTemp);
+
+        String ppPath = ppTemp.getAbsolutePath();
+        String bannerPath = bannerTemp.getAbsolutePath();
+        String s3Url = s3Service.uploadFile(ppName, ppPath);
+        String s3Url2 = s3Service.uploadFile(bannerName, bannerPath);
+        user.setBanner(s3Url2);
+        user.setProfilePic(s3Url);
         return userService.createUser(user);
     }
     @PatchMapping("/update/{id}")
